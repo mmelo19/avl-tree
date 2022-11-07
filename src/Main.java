724fe4ea42caceb8093f16532bc0b108df51019f
Mateus Melo <msmelo@id.uff.br>
2022-11-07 12:00:51 -0300

    Modifica valores na inserção e remoção

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,18 @@ public class Main {
     public static void main(String[] args) {
         AVL tree = new AVL();
 
-        tree.remover(1);
+        tree.inserir(1);
         tree.inserir(10);
         tree.inserir(6);
         tree.inserir(5);
+        tree.inserir(4);
+        tree.inserir(3);
         tree.inserir(20);
-        tree.inserir(32);
-        tree.inserir(19);
+        tree.inserir(40);
+
         tree.print();
 
-        tree.remover(10);
-        tree.remover(20);
+        tree.remover(6);
         tree.print();
     }
 }
